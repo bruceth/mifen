fcb4e72cad501f99053589d19d3de7eb324134d2
Henry <liaohengyi@outlook.com>
2021-03-10 18:21:29 -0500

    yumi

--- src/build.test.ts
+++
@@ -1,8 +0,0 @@
-import { build } from 'tonva-react';
-import { appConfig } from './appConfig';
-
-test('build UqApp', async () => {
-	await build(appConfig);
-	let a = 1;
-	console.log(a);
-}, 600*1000);
