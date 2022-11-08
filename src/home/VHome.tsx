fef3223a6701a5be3e1660c59f13e663b9d0c79a
bruceth <sunbaichuan@hotmail.com>
2022-11-08 15:03:19 +0800

    条款

--- src/home/VHome.tsx
+++ src/home/VHome.tsx
@@ -24,27 +24,35 @@ export class VHome extends VPage<CHome> {
                 <ul className="pr-3 py-2 my-2 bg-white">
                     <li className="my-3">
                         <div className="my-2">交易规则</div>
-                        <ul className="px-3 text-info">
-                            <li><span className="mr-3">规则1: </span>每周最多可以买入账户总值的2%。每次买入一只股票，金额为账户总值的1%或者2%。</li>
-                            <li><span className="mr-3">规则2: </span>同一只股票最多可以买入10次</li>
-                            <li><span className="mr-3">规则3: </span>买入同一只股票股票的时间间隔必须超过12周，即一个季度</li>
-                            <li><span className="mr-3">规则4: </span>买入股票的米息率必须大于5并且高于已经持有股票最低米息率的50%</li>
-                            <li><span className="mr-3">规则5: </span>买入的股票必须通过一票否决条款的审核</li>
-                            <li><span className="mr-3">规则6: </span>股票行业按照大行业为准。强相关的门类算一个行业</li>
-                            <li><span className="mr-3">规则7: </span>一个行业的买入次数不能超过25次。如果发生一次卖出，减一次</li>
-                        </ul>
+                        <ol className="px-3 text-info">
+                            <li><span className="mr-1"></span>每周最多可以买入账户总值的2%。每次买入一只股票，金额为账户总值的1%或者2%。</li>
+                            <li><span className="mr-1"></span>同一只股票最多可以买入总资金的10%</li>
+                            <li><span className="mr-1"></span>买入同一只股票股票的时间间隔必须超过12周，即一个季度</li>
+                            <li><span className="mr-1"></span>买入股票的米息率必须大于5并且高于已经持有股票最低米息率的50%</li>
+                            <li><span className="mr-1"></span>股票行业按照大行业为准。强相关的门类算一个行业</li>
+                            <li><span className="mr-1"></span>一个行业的买入次数不能超过25次。如果发生一次卖出，减一次</li>
+                            <li><span className="mr-1"></span>买入的股票必须通过一票否决条款的审核</li>
+                            <li><span className="mr-1"></span>选择股票依照股票走势条款审核</li>
+                        </ol>
                     </li>
                     <li className="my-3">
                         <div className="my-2">一票否决条款</div>
-                        <ul className="px-3 text-info">
-                            <li><span className="mr-3">条款1: </span>非金融类企业资产负责率超过80%</li>
-                            <li><span className="mr-3">条款2: </span>最近两个季度，营业收入增长连续为负</li>
-                            <li><span className="mr-3">条款3: </span>最近两年ROE连续小于5%</li>
-                            <li><span className="mr-3">条款4: </span>市值小于50亿，美股市值小于10亿</li>
-                            <li><span className="mr-3">条款5: </span>三年内发生欺骗股东的企业丑闻</li>
-                            <li><span className="mr-3">条款6: </span>年报中出现非“标准无保留意见的审计报告”</li>
-                            <li><span className="mr-3">条款7: </span>企业不能长期稳定经营</li>
-                        </ul>
+                        <ol className="px-3 text-info">
+                            <li><span className="mr-1"></span>非金融类企业资产负责率超过80%</li>
+                            <li><span className="mr-1"></span>最近两个季度，营业收入增长连续为负</li>
+                            <li><span className="mr-1"></span>最近两年ROE连续小于5%</li>
+                            <li><span className="mr-1"></span>市值小于50亿，美股市值小于10亿</li>
+                            <li><span className="mr-1"></span>三年内发生欺骗股东的企业丑闻</li>
+                            <li><span className="mr-1"></span>年报中出现非“标准无保留意见的审计报告”</li>
+                            <li><span className="mr-1"></span>企业不能长期稳定经营</li>
+                        </ol>
+                    </li>
+                    <li className="my-3">
+                        <div className="my-2">股票走势条款</div>
+                        <ol className="px-3 text-info">
+                            <li><span className="mr-1"></span>股票价格和60周均线的乖离率最大不能超过15%</li>
+                            <li><span className="mr-1"></span>选择60周均线近两年走平或是向上的股票。</li>
+                        </ol>
                     </li>
                 </ul>
     			{cAccount.renderAccounts()}
