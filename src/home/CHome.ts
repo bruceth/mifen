232791df57a44a81bbf3e2e787e50feca8b89a0a
HenryTi <liaohengyi@gmail.com>
2023-01-20 12:43:35 -0500

    ok

--- src/home/CHome.ts
+++ src/home/CHome.ts
@@ -8,71 +8,71 @@ import { PageItems } from "tonva-react";
 import { VBlog } from "./VBlog";
 
 export class CHome extends CUqBase {
-	readonly cAccount: CAccount;
-	stocks: IObservableArray<Stock & StockValue> = null;
-	listCaption: string = null;
-	blogs: BlogPageItems;
-	blog: Blog;
+    readonly cAccount: CAccount;
+    stocks: IObservableArray<Stock & StockValue> = null;
+    listCaption: string = null;
+    blogs: BlogPageItems;
+    blog: Blog;
 
-	constructor(cApp: CApp) {
-		super(cApp);
-		makeObservable(this, {
-			stocks: observable.ref,
-			listCaption: observable,
-		});
-		this.cAccount = this.newSub(CAccount);
-	}
+    constructor(cApp: CApp) {
+        super(cApp);
+        makeObservable(this, {
+            stocks: observable.ref,
+            listCaption: observable,
+        });
+        this.cAccount = this.newSub(CAccount);
+    }
 
-	protected async internalStart(param: any) {
-		this.openVPage(VHome);
-	}
+    protected async internalStart(param: any) {
+        this.openVPage(VHome);
+    }
 
-	tab = () => {
-		return this.renderView(VHome);
-	}
+    tab = () => {
+        return this.renderView(VHome);
+    }
 
-	onStockClick = async (stock: Stock) => {
-		this.cApp.cCommon.showStock(stock);
-	}
+    onStockClick = async (stock: Stock) => {
+        this.cApp.cCommon.showStock(stock);
+    }
 
-	showBlogs = async () => {
-		this.blogs = new BlogPageItems(this.uqs.BruceYuMi);
-		this.openVPage(VBlogs);
-		await this.blogs.first(undefined);
-	}
+    showBlogs = async () => {
+        this.blogs = new BlogPageItems(this.uqs.BruceYuMi);
+        this.openVPage(VBlogs);
+        await this.blogs.first(undefined);
+    }
 
-	onClickBlog(blog: Blog) {
-		this.blog = blog;
-		this.openVPage(VBlog);
-	}
+    onClickBlog(blog: Blog) {
+        this.blog = blog;
+        this.openVPage(VBlog);
+    }
 
-	showIntroduction = () => {
-		this.cApp.cMe.showFaq();
-	}
+    showIntroduction = () => {
+        this.cApp.cMe.showFaq();
+    }
 }
 
 
 export class BlogPageItems extends PageItems<Blog> {
-	private yumi: BruceYuMi.UqExt;
+    private yumi: BruceYuMi.UqExt;
 
-	constructor(yumi: BruceYuMi.UqExt) {
-		super(false);
-		this.yumi = yumi;
-		this.firstSize = 30;
-		this.pageSize = 10;
-	}
-	
-	protected getPageId(item:Blog):any {
-		return item?.id;
-	}
+    constructor(yumi: BruceYuMi.UqExt) {
+        super(false);
+        this.yumi = yumi;
+        this.firstSize = 30;
+        this.pageSize = 10;
+    }
 
-	protected async loadResults (param:undefined, pageStart:any, pageSize:number):Promise<{[name:string]:any[]}> {
-		let ret = await this.yumi.ID<Blog>({
-			IDX: this.yumi.Blog,
-			id: undefined,
-			order: 'desc',
-			page: {start: pageStart, size: pageSize},
-		});
-		return {$page: ret};
-	}
+    protected getPageId(item: Blog): any {
+        return item?.id;
+    }
+
+    protected async loadResults(param: undefined, pageStart: any, pageSize: number): Promise<{ [name: string]: any[] }> {
+        let ret = await this.yumi.ID<Blog>({
+            IDX: this.yumi.Blog,
+            id: undefined,
+            order: 'desc',
+            page: { start: pageStart, size: pageSize },
+        });
+        return { $page: ret };
+    }
 }
